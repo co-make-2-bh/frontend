cd365abb29933eacc732966e8da9021298ef5d6a
EmilyA <emilyadams.3d@gmail.com>
2020-09-24 10:41:11 -0700

    Revert "Fixed the login problems"
    This reverts commit f49311b80a6610205614a6f273ec827572ecac4e.

--- co-make/src/Components/Upvote.js
+++ co-make/src/Components/Upvote.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Upvote(props) {
   const { upvote, setUpvote } = props;
@@ -7,10 +7,7 @@ export default function Upvote(props) {
     e.preventDefault();
     let count = 1 + upvote;
     setUpvote(count);
-    //axiosWithAuth().patch("/issues/issue:id/upvote")
   };
-  //useEffect(let count = 1 + upvote;
-  //.get)
 
   return <button onClick={upvotePost}>Upvote</button>;
 }
